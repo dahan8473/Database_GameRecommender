cc28a59406179553fa02e5984b22416affd2d3cc
Ethan Liu <108840760+EthanL818@users.noreply.github.com>
2024-12-01 19:52:49 -0500

    ??

---
+++ SRC/server/server.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "videogamereccomender",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as ID", connection.threadId);
+});
